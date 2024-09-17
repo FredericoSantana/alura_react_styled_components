099da22f0d400c2cbc5b6f6e93d6f1fc106f2fe0
FredericoSantana <fredericosantana@hotmail.com.br>
2024-09-17 16:31:29 -0300

    criação de estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import GlobalStyles from "./components/GlobalStyles";
 
 const GradientBackground = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <GradientBackground>
-      tchan
+      <GlobalStyles/>
     </GradientBackground>
   )
 }
