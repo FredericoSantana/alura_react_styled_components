1d3fb2ac21f3862693c41ce6d2d7af31b83aee8b
FredericoSantana <fredericosantana@hotmail.com.br>
2024-09-18 20:06:48 -0300

    estilização das tags

--- src/components/Gallery/Tags/index.jsx
+++ src/components/Gallery/Tags/index.jsx
@@ -1,10 +1,32 @@
 import tags from "./tags.json";
+import styled from "styled-components";
+
+const TagTitle = styled.h3({
+  color: "#D9D9D9",
+  fontSize: "24px",
+  margin: 0
+});
+
+const Tag = styled.button({
+  fontSize: "24px",
+  color: "#FFFFFF",
+  background: "rgba(217, 217, 217, 0.3)",
+  borderRadius: "10px",
+  cursor: "pointer",
+  transition: "background-color 0.3s ease",
+  padding: "12px",
+  boxSizing: "border-box",
+  border: "2px solid transparent",
+  "&:hover": {
+    borderColor: "#C98CF1",
+  }
+});
 
 const Tags = () => {
   return (
     <>
-      <p>Busque por tags:</p>
-      {tags.map(tag => <button key={tag.id}>{tag.titulo}</button>)}
+      <TagTitle>Busque por tags:</TagTitle>
+      {tags.map(tag => <Tag key={tag.id}>{tag.titulo}</Tag>)}
     </>
   );
 };
