a6dc2b65f7a1bd651826db581606b8f8c869d88b
FredericoSantana <fredericosantana@hotmail.com.br>
2024-09-19 12:25:39 -0300

    estilização do modal e da seção populares

--- src/components/Gallery/Popular/index.jsx
+++ src/components/Gallery/Popular/index.jsx
@@ -1,10 +1,41 @@
 import Title from "../../Title";
+import styled from "styled-components";
+import photos from "./fotos-populares.json";
+
+const PhotosColumn = styled.section({
+  display: "flex",
+  flexDirection: "column",
+  gap: "16px",
+});
+
+const Image = styled.img({
+  maxWidth: "212px",
+  borderRadius: "20px",
+});
+
+const Button = styled.button({
+  backgroundColor: "transparent",
+  color: "#fff",
+  border: "2px solid",
+  borderColor: "#C98CF1",
+  padding: "12px 20px",
+  fontSize: "20px",
+  borderRadius: "10px",
+  cursor: "pointer",
+  width: "100%",
+  marginTop: "16px",
+});
 
 const Popular = () => {
   return (
-    <Title alignment>
-      Populares
-    </Title>
+    <section>
+      <Title alignment="center">Populares</Title>
+      <PhotosColumn>
+        {photos.map(photo => <Image key={photo.id} src={photo.path} alt={photo.alt}/>)}
+      </PhotosColumn>
+      <Button>Ver mais</Button>
+    </section>
+
   );
 };
 
--- src/components/ModalZoom/index.jsx
+++ src/components/ModalZoom/index.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
+import Image from "../Gallery/Image";
+import styled from "styled-components";
+import IconButton from "../IconButton";
 
-const ModalZoom = ({ photo }) => {
+const Overlay = styled.div({
+  backgroundColor: "rgba(0, 0, 0, 0.7)",
+  position: "fixed",
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+});
+
+const StyledDialog = styled.dialog({
+  position: "absolute",
+  top: "294px",
+  background: "transparent",
+  padding: 0,
+  border: 0,
+  width: "1156px",
+  display: "flex",
+  justifyContent: "center",
+  form: {
+    button: {
+      position: "relative",
+      top: "20px",
+      right: "60px",
+    }
+  }
+})
+
+const ModalZoom = ({photo, aoFechar}) => {
   return (
     <>
-      <dialog open={!!photo}>
-        <p>gasçlgas</p>
-        <form method="dialog">
-          <button>ok</button>
-        </form>
-      </dialog>
+      {photo && <>
+        <Overlay/>
+        <StyledDialog open={!!photo} onClose={aoFechar}>
+          <Image photo={photo} expanded={true}/>
+          <form method="dialog">
+            <IconButton formMethod="dialog">
+              <img src="/icons/fechar.png" alt="Icone de fechar" />
+            </IconButton>
+          </form>
+        </StyledDialog>
+      </>}
     </>
   );
 };
